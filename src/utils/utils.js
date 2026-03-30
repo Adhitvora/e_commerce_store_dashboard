0587cb5ce9f55f5e7cd0ce245d50ae1fd0321477
Adhitvora <voraadhit2096@gmail.com>
2026-03-30 10:56:00 +0530

    feat: correct production URLs for app and API

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -16,8 +16,8 @@ const mode = process.env.REACT_APP_MODE;
 let app_url, api_url
 
 if (mode === production) {
-    app_url = 'https://myhaat24.com';
-    api_url = 'https://seller.myhaat24.com';
+    app_url = 'https://seller.myhaat24.com';
+    api_url = 'https://api.myhaat24.com';
 } else {
     app_url = 'http://localhost:3001'
     api_url = 'http://localhost:5000'
